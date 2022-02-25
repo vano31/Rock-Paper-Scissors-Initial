148df32f99a9759d28a0286925d6b61300f26fd7
vano31 <vanosike31@gmail.com>
2022-02-25 14:03:06 -0500

    add compareSelection function to decide winner of RPS round.

--- script.js
+++ script.js
@@ -22,20 +22,19 @@ function startRound() {
     if (playerSelection === 0) {
         alert (`User Input Not Valid. Computer Wins Round.`);
     } else {
-        //here is where I will put the function randomRPS
+        randomRPS();
+        //this is where you put compareSelection():
+        compareSelection();
     }
 
 }
 
+startRound();
 
 function randomRPS() {
-
-
     let randomNumber = randomGenerator();
-
     randomGenerator();
-
-    getRandomGenerator();
+    getRandomGenerator(); 
 
     function randomGenerator() {
         return Math.random();
@@ -61,18 +60,44 @@ function randomRPS() {
 
 
 
-randomRPS();
+
+function compareSelection() {
+
+    switch (playerSelection) {
+        case `ROCK`:
+            if (computerSelection === `PAPER`) {
+                alert(`Computer chose PAPER. You lose this round. PAPER beats ROCK.`);
+            }   else if (computerSelection === `SCISSORS`) {
+                alert(`Computer chose SCISSORS. You win this round! ROCK beats SCISSORS.`);
+            }   else {
+                alert(`Tie! You and Computer both chose ROCK.`);
+            }
+            break;
 
 
-//Below code is test
+        case `PAPER`:
+            if (computerSelection === `SCISSORS`) {
+                alert(`Computer chose SCISSORS. You lose this round. SCISSORS beat PAPER.`);
+            }   else if (computerSelection === `ROCK`) {
+                alert(`Computer chose ROCK. You win this round! PAPER beats ROCK.`);
+            }   else {
+                alert(`Tie! You and Computer both chose PAPER.`);
+            }
+            break;
 
 
+        case `SCISSORS`:
+            if (computerSelection === `ROCK`) {
+                alert(`Computer chose ROCK. You lose this round. ROCK beats SCISSORS.`);
+            }   else if (computerSelection === `PAPER`) {
+                alert(`Computer chose PAPER. You win this round! SCISSORS beats PAPER.`);
+            }   else {
+                alert(`Tie! You and Computer both chose SCISSORS.`);
+            }
+            break;
 
 
+    }
 
-
-
-
-startRound();
-
+}
 

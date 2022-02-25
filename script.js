b8278697ad0efcc0f2b1fd273be68d41713950f7
vano31 <vanosike31@gmail.com>
2022-02-25 18:05:47 -0500

    Add for loop for ound calculations. However, for loop wont start

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //Game of Rock, Paper, Scissors
 
-let answer = prompt(`Please write either Rock, Paper, or Scissors.`);
+let answer = prompt(`Please write either Rock, Paper, or Scissors. Win 3 out of 5 rounds to win the match!`);
 let playerSelection = answer.toUpperCase();
 
 function selectionChecker () {
@@ -17,19 +17,46 @@ selectionChecker();
 
 let computerSelection;
 let roundNumber;
+let computerWins = 0;
+let userWins = 0;
+
+
+
+
+for (let roundNumber = 0; (roundNumber > 5 || computerWins === 3 || userWins === 3); ++roundNumber) {
+    startRound();
+    //Below: repeat prompt for answer. Honestly should have made that a function.
+    answer = prompt(`Please write either Rock, Paper, or Scissors. You are on Round ${roundNumber}`);
+
+}
+
+if (computerWins === 3) {
+    alert(`Sorry...The Computer won 3 out of 5 matches`);
+    } else if (userWins === 3) {
+        alert (`Congratulations! You won 3 out of 5 matches!`);
+    } else if (roundNumber === 5) {
+        alert (`Looks like it was a tie!`);
+ }
+
+
+
+
+
 
 function startRound() {
     if (playerSelection === 0) {
         alert (`User Input Not Valid. Computer Wins Round.`);
+        computerWins += 1;
     } else {
         randomRPS();
         //this is where you put compareSelection():
         compareSelection();
+        //will put loop for variable roundNumber to iniitiate whenever 
     }
 
 }
 
-startRound();
+//startRound(); <-- this needs to be  removed and put in the for statement above 
 
 function randomRPS() {
     let randomNumber = randomGenerator();
@@ -67,8 +94,10 @@ function compareSelection() {
         case `ROCK`:
             if (computerSelection === `PAPER`) {
                 alert(`Computer chose PAPER. You lose this round. PAPER beats ROCK.`);
+                computerWins += 1;
             }   else if (computerSelection === `SCISSORS`) {
                 alert(`Computer chose SCISSORS. You win this round! ROCK beats SCISSORS.`);
+                userWins += 1;
             }   else {
                 alert(`Tie! You and Computer both chose ROCK.`);
             }
@@ -78,8 +107,10 @@ function compareSelection() {
         case `PAPER`:
             if (computerSelection === `SCISSORS`) {
                 alert(`Computer chose SCISSORS. You lose this round. SCISSORS beat PAPER.`);
+                computerWins += 1;
             }   else if (computerSelection === `ROCK`) {
                 alert(`Computer chose ROCK. You win this round! PAPER beats ROCK.`);
+                userWins += 1;
             }   else {
                 alert(`Tie! You and Computer both chose PAPER.`);
             }
@@ -89,8 +120,10 @@ function compareSelection() {
         case `SCISSORS`:
             if (computerSelection === `ROCK`) {
                 alert(`Computer chose ROCK. You lose this round. ROCK beats SCISSORS.`);
+                computerWins += 1;
             }   else if (computerSelection === `PAPER`) {
                 alert(`Computer chose PAPER. You win this round! SCISSORS beats PAPER.`);
+                userWins += 1;
             }   else {
                 alert(`Tie! You and Computer both chose SCISSORS.`);
             }
@@ -98,6 +131,5 @@ function compareSelection() {
 
 
     }
-
 }
 
